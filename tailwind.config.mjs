ada71e725eae49ce865fb4fadb0aebfcac1be715
Sofie <sohe0002@stud.kea.dk>
2024-10-27 12:01:04 +0100

    css og første tilføjelser

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,30 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+            colors: {
+                rød: "#FF1054",
+                grøn: "#008481",
+                lysgrå: "#CACDD5",
+                grå: "#70727E",
+                hvid: "#FFFFFF",
+            },
+
+            fontFamily: {
+                sigmar: ['"Sigmar"', 'sans-serif'],
+                rubik: ['"Rubik"', 'sans-serif'],
+              },
+              
+              animation: {
+                'infinite-scroll': 'infinite-scroll 25s linear infinite',
+              },
+              keyframes: {
+                'infinite-scroll': {
+                  from: { transform: 'translateX(0)' },
+                  to: { transform: 'translateX(-100%)' },
+                },
+              }
+        },
 	},
 	plugins: [
         require('@tailwindcss/typography'),
